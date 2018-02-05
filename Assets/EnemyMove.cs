53722f609531900a4809ec122d09a633e1d184ab
WoongPyo Hong <hongup1@naver.com>
2018-02-05 17:38:32 +0900

    2018.02.05 - Hodge

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+    public float enemySpeed = 3.0f;
+    public GameObject player;
+
+    private Vector3 movingVector;
+
+    void Start () {
+        player = GameObject.Find("Player/Cube");
+    }
+
+    void Update () {
+        var diff = player.transform.position- transform.position;
+        movingVector = diff.normalized * enemySpeed * Time.deltaTime;
+        transform.Translate(movingVector);
+	}
+    
+}
---
+++ Assets/triggers.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class triggers : MonoBehaviour {
+
+    public Text testText;
+    public float health = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Ground") {
+            Destroy(other.transform.parent.gameObject);
+        }
+        testText.text = string.Format("{0}", health--);
+    }
+}
